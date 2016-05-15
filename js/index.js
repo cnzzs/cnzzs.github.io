915cefefb6e1ded357a686ba411ca09453d6481b
cnzzs <cnzhengzs@gmail.com>
2016-05-15 12:42:45 +0800

    Initialize

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ZaoSheng on 2016/5/15.
+ */
+if(!window.slider) {
+    var slider={};
+}
+
+slider.data= [
+    {
+        "id":"slide-img-1", // 与slide-runner中的img标签id对应
+        "client":"标题1",
+        "desc":"这里修改描述 这里修改描述 这里修改描述" //这里修改描述
+    },
+    {
+        "id":"slide-img-2",
+        "client":"标题2",
+        "desc":"add your description here"
+    },
+    {
+        "id":"slide-img-3",
+        "client":"标题3",
+        "desc":"add your description here"
+    }
+];
